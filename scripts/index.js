70138fa2c966cd587d81539472321f92e24e83a6
Tatyana Ak <akulincinata@yandex.ru>
2020-07-07 15:12:43 +0300

    fix: js,Button - поведение кнопок,

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,31 @@
-const openEditBatton = document.querySelector('.profile-info__edit-button');
-const closeEditBatton = document.querySelector('.form__close-icon');
+const openEditbutton = document.querySelector('.profile-info__edit-button');
+let closeEditbutton = document.querySelector('.form__close-icon');
 let profileInfo = document.querySelector('.profile-info');
 let editForm = document.querySelector('.edit-form');
+let Form = document.querySelector('.form');
 let name = profileInfo.querySelector('.profile-info__name');
 let memo = profileInfo.querySelector('.profile-info__memo');
 let inputName = editForm.querySelector('.form__item_name');
 let inputMemo = editForm.querySelector('.form__item_memo');
-const battonSave = editForm.querySelector('.form__batton');
 
-function editFormToggle() {
+
+function editFormToggle(event) {
+    event.preventDefault();
     editForm.classList.toggle('edit-form_open');
-    inputName.value = name.textContent;
-    inputMemo.value = memo.textContent;
+    if (editForm.classList.contains('edit-form_open')) {
+        inputName.value = name.textContent;
+        inputMemo.value = memo.textContent;
+    }
+    
 }
 function saveProfile(event) {
     event.preventDefault();
     name.textContent = inputName.value;
     memo.textContent = inputMemo.value;
-    editFormToggle();
+    editFormToggle(event);
 }
-openEditBatton.addEventListener('click', editFormToggle);
-closeEditBatton.addEventListener('click', editFormToggle);
+openEditbutton.addEventListener('click', editFormToggle);
+closeEditbutton.addEventListener('click', editFormToggle);
 
-battonSave.addEventListener('click',saveProfile );
+Form.addEventListener('submit',saveProfile );
 
