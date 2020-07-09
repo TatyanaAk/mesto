f3eab55cdbe05eeb7c9b7e34cddbaec14efea211
Tatyana Ak <akulincinata@yandex.ru>
2020-07-09 13:44:53 +0300

    fix: поправила стили, js, прописала тип кнопкам

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,15 @@
-const openEditbutton = document.querySelector('.profile-info__edit-button');
-let closeEditbutton = document.querySelector('.form__close-icon');
+const openEditButton = document.querySelector('.profile-info__edit-button');
+const closeEditButton = document.querySelector('.form__close-icon');
 let profileInfo = document.querySelector('.profile-info');
 let editForm = document.querySelector('.edit-form');
-let Form = document.querySelector('.form');
+let form = editForm.querySelector('.form');
 let name = profileInfo.querySelector('.profile-info__name');
 let memo = profileInfo.querySelector('.profile-info__memo');
-let inputName = editForm.querySelector('.form__item_name');
-let inputMemo = editForm.querySelector('.form__item_memo');
+let inputName = form.querySelector('.form__item_name');
+let inputMemo = form.querySelector('.form__item_memo');
 
 
-function editFormToggle(event) {
-    event.preventDefault();
+function editFormToggle() {
     editForm.classList.toggle('edit-form_open');
     if (editForm.classList.contains('edit-form_open')) {
         inputName.value = name.textContent;
@@ -22,10 +21,10 @@ function saveProfile(event) {
     event.preventDefault();
     name.textContent = inputName.value;
     memo.textContent = inputMemo.value;
-    editFormToggle(event);
+    editFormToggle();
 }
-openEditbutton.addEventListener('click', editFormToggle);
-closeEditbutton.addEventListener('click', editFormToggle);
+openEditButton.addEventListener('click', editFormToggle);
+closeEditButton.addEventListener('click', editFormToggle);
 
-Form.addEventListener('submit',saveProfile );
+form.addEventListener('submit',saveProfile);
 
