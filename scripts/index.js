b2c93fa8562cfce8d96fca77bc0e8a82e5342c43
Tatyana Ak <akulincinata@yandex.ru>
2020-07-18 18:38:09 +0300

    feat: создала карточки в JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,29 @@
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
 const openEditButton = document.querySelector('.profile__edit-button');
 const closeEditButton = document.querySelector('.edit-form__close-icon');
 let profileInfo = document.querySelector('.profile__profile-info');
@@ -28,3 +54,13 @@ closeEditButton.addEventListener('click', editFormToggle);
 
 form.addEventListener('submit',saveProfile);
 
+const gridTemplate = document.querySelector('#card').content;
+const gridMenu = document.querySelector('.grid__menu');
+for (let i = 0; i < initialCards.length; i += 1) {
+    // каждый раз создавать новый клон элемента
+    const gridElement = gridTemplate.cloneNode(true);
+    gridElement.querySelector('.grid__grid-image').src=initialCards[i].link;
+    gridElement.querySelector('.grid__grid-image').alt = initialCards[i].name;
+    gridElement.querySelector('.grid__title').textContent = initialCards[i].name;
+    gridMenu.append(gridElement);
+}
