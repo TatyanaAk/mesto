3108b171fef8e0cf13293fa75b3141c7fe55fc19
Tatyana Ak <akulincinata@yandex.ru>
2020-07-21 22:34:48 +0300

    refactor: оптимизировала создание карточек

--- scripts/index.js
+++ scripts/index.js
@@ -31,16 +31,21 @@ let profileInfo = document.querySelector('.profile__profile-info');
 let editForm = document.querySelector('.edit-form_profile');
 let cardForm = document.querySelector('.edit-form_card');
 let cardZoom = document.querySelector('.edit-form_card-zoom');
+// кнопки закрытия модалок
 const closeEditForm = editForm.querySelector('.edit-form__close-icon');
 const closeAddCard = cardForm.querySelector('.edit-form__close-icon');
 const closeImage = cardZoom.querySelector('.edit-form__close-icon');
+// элементы профиля
 const form = editForm.querySelector('.edit-form__form');
 let name = profileInfo.querySelector('.profile__name');
 let memo = profileInfo.querySelector('.profile__memo');
 let inputName = form.querySelector('.edit-form__item_name');
 let inputMemo = form.querySelector('.edit-form__item_memo');
+// элементы модалки создания карточки
 let inputTitle = cardForm.querySelector('.edit-form__item_title');
 let inputLink = cardForm.querySelector('.edit-form__item_link');
+const gridTemplate = document.querySelector('#card').content;
+const gridCards = document.querySelector('.grid__cards');
 
 
 function formToggle(modalWindow) {
@@ -70,6 +75,32 @@ function saveProfile(event) {
     memo.textContent = inputMemo.value;
     formToggle(editForm);
 }
+// renderCard - рисует карточку.
+function renderCard(data) {
+    const gridElement = gridTemplate.cloneNode(true);
+    gridElement.querySelector('.grid__grid-image').src = data.link;
+    gridElement.querySelector('.grid__grid-image').alt = data.name;
+    gridElement.querySelector('.grid__title').textContent = data.name;
+    gridElement.querySelector('.grid__heart').addEventListener('click', function (evt) {
+        evt.target.classList.toggle('grid__heart_active');
+    });
+    const deleteButton = gridElement.querySelector('.grid__basket');
+    deleteButton.addEventListener('click', function () {
+        const listItem = deleteButton.closest('.grid__element');
+        listItem.remove();
+    });
+    gridElement.querySelector('.grid__grid-image').addEventListener('click', editFormZoom);
+    gridCards.prepend(gridElement);
+}
+// addCard - добавляет карточку в grid__cards
+function addCard(event) {
+    event.preventDefault();
+    if (inputLink.value !== '') {    
+        renderCard({name: inputTitle.value, link: inputLink.value });
+    }
+    formToggle(cardForm);
+}
+//устновка обработчика событий на кнопки.
 openEditButton.addEventListener('click', () => {
     formToggle(editForm);
 });
@@ -85,48 +116,10 @@ closeAddCard.addEventListener('click', () => {
 closeImage.addEventListener('click', () => {
     formToggle(cardZoom);
 });
-
 form.addEventListener('submit',saveProfile);
-
 cardForm.querySelector('.edit-form__form').addEventListener('submit',addCard);
-
-
-const gridTemplate = document.querySelector('#card').content;
-const gridCards = document.querySelector('.grid__cards');
+//создание карточек по умолчанию.
 for (let i = 0; i < initialCards.length; i += 1) {
-    // каждый раз создавать новый клон элемента
-    const gridElement = gridTemplate.cloneNode(true);
-    gridElement.querySelector('.grid__grid-image').src = initialCards[i].link;
-    gridElement.querySelector('.grid__grid-image').alt = initialCards[i].name;
-    gridElement.querySelector('.grid__title').textContent = initialCards[i].name;
-    gridElement.querySelector('.grid__heart').addEventListener('click', function (evt) {
-        evt.target.classList.toggle('grid__heart_active');
-    });
-    const deleteButton = gridElement.querySelector('.grid__basket');
-    deleteButton.addEventListener('click', function () {
-        const listItem = deleteButton.closest('.grid__element');
-        listItem.remove();
-    });
-    gridElement.querySelector('.grid__grid-image').addEventListener('click', editFormZoom);
-    gridCards.append(gridElement);
+    renderCard(initialCards[i]);
 }
-function addCard(event) {
-    event.preventDefault();
-    if (inputLink.value !== '') {
-        const gridElement = gridTemplate.cloneNode(true);
-        gridElement.querySelector('.grid__grid-image').src = inputLink.value;
-        gridElement.querySelector('.grid__grid-image').alt = inputTitle.value;
-        gridElement.querySelector('.grid__title').textContent = inputTitle.value;
-        gridElement.querySelector('.grid__heart').addEventListener('click', function (evt) {
-            evt.target.classList.toggle('grid__heart_active');
-        });
-        const deleteButton = gridElement.querySelector('.grid__basket');
-        deleteButton.addEventListener('click', function () {
-            const listItem = deleteButton.closest('.grid__element');
-            listItem.remove();
-        });
-        gridElement.querySelector('.grid__grid-image').addEventListener('click', editFormZoom);
-        gridCards.prepend(gridElement);
-    }
-    formToggle(cardForm);
-}
+
